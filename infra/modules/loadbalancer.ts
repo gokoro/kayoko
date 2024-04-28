74875f3c21c2bafb2536bbbb97d04cf7082f008b
Lincroe <lincroe@gmail.com>
2024-04-28 17:53:34 +0900

    feat: proxy for the certain community
    commit f4bee1e31a78356768c0cd275ef2ab9fe88eab04
    Author: gokoro <lincroe@gmail.com>
    Date:   Wed Jan 17 21:49:30 2024 +0900
    
        chore: update path usage of proxy
    
    commit 62e0121659cd7c87f6be62087034b9e2bc4248c1
    Author: gokoro <lincroe@gmail.com>
    Date:   Wed Jan 17 01:48:34 2024 +0900
    
        feat: add replacement with proxy for a certain community
    
    commit a3515a4729b36ad8699997c0e206e1835fc75c0b
    Author: gokoro <lincroe@gmail.com>
    Date:   Sat Nov 4 11:48:31 2023 +0900
    
        Add resource for sd
    
    commit 29cffe7051fa62c085cec2c937e0ff5ffd28497c
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 23:57:34 2023 +0900
    
        fix
    
    commit 62738052fbe4dbfe0093f6cf94e5b9ef8193c3b9
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 22:04:16 2023 +0900
    
        remove sd from action
    
    commit 170db251310a5ba81843abd8eb1e90ef7d092794
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 22:04:03 2023 +0900
    
        for codebuild
    
    commit 881694e56568fa0d347e112d356c96647d3b6cb5
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 21:16:12 2023 +0900
    
        Add sd
    
    commit 2c5f801fa203582df2a49e7d24522b1a58da2aa1
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 21:07:06 2023 +0900
    
        fix
    
    commit 3945ea85461404e0e9704551268e58c288075c1f
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 19:51:07 2023 +0900
    
        fix
    
    commit 4767d7e9c373f24f0a050fc75512b894f434d939
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 06:27:22 2023 +0900
    
        fix
    
    commit ac52f182d8f7d6f91470c5e8ce02b91243458651
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 06:23:53 2023 +0900
    
        fix
    
    commit 722f90cb795512c4377acb1a3f0e940d95393b27
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 05:51:36 2023 +0900
    
        fix
    
    commit 75de544ea52fdc03de182733e7407681832157b3
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 05:47:05 2023 +0900
    
        fix
    
    commit 49d4deee75ed40ccbdc274c4d689565ec462e542
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 05:45:34 2023 +0900
    
        test
    
    commit f6c7d861c06e94108655b81f988c9d1846d068a4
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 04:10:20 2023 +0900
    
        remove healthcheck
    
    commit 5db56f9a040ffde70b4e03bdf9c383d8d9dfb4b5
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 03:27:54 2023 +0900
    
        add healthcheck
    
    commit fa3dea9d30fa5a1cff752a577d231bbb1685333b
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 02:02:02 2023 +0900
    
        fix
    
    commit 74b9b4d63e8bc5f626b294e357cb0aece7b5f8c7
    Author: gokoro <lincroe@gmail.com>
    Date:   Mon Oct 23 00:56:23 2023 +0900
    
        Specify host to dockerfile

---
+++ infra/modules/loadbalancer.ts
@@ -0,0 +1,38 @@
+import * as pulumi from '@pulumi/pulumi'
+import * as aws from '@pulumi/aws'
+
+import { vpc, subnets } from './vpc'
+
+const vpcId = vpc.id
+
+export const securityGroups = new aws.ec2.SecurityGroup(
+  'kayoko-lb-security-group',
+  {
+    vpcId,
+    ingress: [
+      { protocol: 'tcp', fromPort: 80, toPort: 80, cidrBlocks: ['0.0.0.0/0'] },
+      {
+        protocol: 'tcp',
+        fromPort: 443,
+        toPort: 443,
+        cidrBlocks: ['0.0.0.0/0'],
+      },
+    ],
+    egress: [
+      {
+        fromPort: 0,
+        toPort: 0,
+        protocol: '-1',
+        cidrBlocks: ['0.0.0.0/0'],
+        ipv6CidrBlocks: ['::/0'],
+      },
+    ],
+  }
+)
+
+export const lb = new aws.lb.LoadBalancer('kayoko-loadbalancer', {
+  internal: false,
+  loadBalancerType: 'application',
+  securityGroups: [securityGroups.id],
+  subnets: subnets.map((subnet) => subnet.id),
+})
