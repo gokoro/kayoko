eb9f05320f787605c91f89d72bf8eee777a8dc28
Lincroe <lincroe@gmail.com>
2025-01-02 00:57:10 +0900

    temp

--- client/src/chats/utils.ts
+++
@@ -1,9 +0,0 @@
-// import type { ApplicationCommandStructure } from 'eris'
-
-// import type { ClientType } from '../libs/eris.js'
-
-// import { config } from '../configs/index.js'
-
-// export const createCommandInitializer = (bot: ClientType, struct: ApplicationCommandStructure) => {
-//   return () => bot.createGuildCommand(config.DISCORD_DEFAULT_GUILD_ID, struct)
-// }
