6c3e5db93f616ff65d4bac97495aca5265e6c17c
Lincroe <lincroe@gmail.com>
2025-01-02 03:01:15 +0900

    add proxy for instagram

--- client/src/chats/proxys/handlers.ts
+++ client/src/chats/proxys/handlers.ts
@@ -19,3 +19,21 @@ export const ArcaHandler: MessageCreationHandler = (bot, message) => {
     bot.deleteMessage(message.channel.id, message.id)
   }
 }
+
+export const InstagramHandler: MessageCreationHandler = (bot, message) => {
+  const origin = 'https://www.instagram.com'
+
+  if (message.content.startsWith(origin) && !message.content.includes('/share/')) {
+    const target = message.content.replace(origin, 'https://www.ddinstagram.com')
+    const content = `${message.author.mention} - ${target}`
+
+    bot.createMessage(message.channel.id, {
+      content,
+      allowedMentions: {
+        users: true,
+      },
+    })
+
+    bot.deleteMessage(message.channel.id, message.id)
+  }
+}
--- client/src/chats/proxys/index.ts
+++ client/src/chats/proxys/index.ts
@@ -3,10 +3,10 @@ import { Constants } from 'eris'
 import type { ClientType } from '../../libs/eris.js'
 import type { RegisterModule, RegisterModuleReturnedContext } from '../types.js'
 
-import { ArcaHandler } from './handlers.js'
+import { ArcaHandler, InstagramHandler } from './handlers.js'
 
 const createContext = (): RegisterModuleReturnedContext => ({
-  messageCreation: [{ handler: ArcaHandler }],
+  messageCreation: [{ handler: ArcaHandler }, { handler: InstagramHandler }],
 })
 
 export const proxys: RegisterModule = (bot) => {
