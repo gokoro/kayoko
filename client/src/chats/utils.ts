7b3d3d100d88038af0c5276823f4727255032484
Lincroe <lincroe@gmail.com>
2025-01-01 21:53:08 +0900

    remove unused method

--- client/src/chats/utils.ts
+++ client/src/chats/utils.ts
@@ -4,9 +4,6 @@ import type { ClientType } from '../libs/eris.js'
 
 import { config } from '../configs/index.js'
 
-export const createCommandInitializer = (
-  bot: ClientType,
-  struct: ApplicationCommandStructure
-) => {
-  return () => bot.createGuildCommand(config.DISCORD_DEFAULT_GUILD_ID, struct)
-}
+// export const createCommandInitializer = (bot: ClientType, struct: ApplicationCommandStructure) => {
+//   return () => bot.createGuildCommand(config.DISCORD_DEFAULT_GUILD_ID, struct)
+// }
