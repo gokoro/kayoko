47771777aacb4b51c3699a4cf3976cf6999e3f8a
Lincroe <lincroe@gmail.com>
2025-01-03 18:26:44 +0900

    chore: update logic

--- client/src/chats/proxys/handlers.ts
+++ client/src/chats/proxys/handlers.ts
@@ -6,6 +6,36 @@ import { config } from '../../configs/index.js'
 
 const origin = 'https://arca.live'
 
+// Only filters profile routes;
+// Invalid routes will still shown as supported
+function isRouteToProfile(url: string) {
+  // Gather minimum search range
+  // 1. This will cut off possible trailing slash
+  url = url.slice(20 /* 'http://instagram.com'.length */, 56 /* 'https://www.instagram.com/'.length + 30 */)
+
+  // Fast exit in case of minimum username length hit
+  if (url.length < 2 /* '/a' */) {
+    return false
+  }
+
+  for (let pos = 0, seen = 0; pos < url.length; pos++) {
+    // Supported in case of
+    // 1. count of accumulative slashes of geq 2
+    // 2. not a trailing slash
+    if (
+      url.charCodeAt(pos) === 47 /* '/'.charCodeAt() */ &&
+      ++seen >= 2 &&
+      url.charCodeAt(++pos) !== 63 /* '?'.charCodeAt() */ &&
+      // Note `pos` was increased right before this evaluation
+      pos !== url.length
+    ) {
+      return false
+    }
+  }
+
+  return true
+}
+
 export const ArcaHandler: MessageCreationHandler = (bot, message) => {
   if (message.content.startsWith(origin)) {
     const target = message.content.replace(origin, '')
@@ -26,10 +56,7 @@ export const InstagramHandler: MessageCreationHandler = (bot, message) => {
   const origin = 'https://www.instagram.com'
 
   if (message.content.startsWith(origin) && !message.content.includes('/share/')) {
-    const url = new URL(message.content)
-    const [, ...paths] = url.pathname.replace(/\/$/, '').split('/')
-
-    if (paths.length <= 1) return
+    if (isRouteToProfile(message.content)) return
 
     const target = message.content.replace(origin, 'https://www.ddinstagram.com')
     const content = `${message.author.mention} - ${target}`
